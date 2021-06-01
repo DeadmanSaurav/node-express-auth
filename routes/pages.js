20febe1bfd2054d2411642d6950edace805f896c
Saurav Srivastav <46235772+SauravSrivastav22@users.noreply.github.com>
2021-06-01 11:10:34 +0530

    Second Commit

--- routes/pages.js
+++ routes/pages.js
@@ -1,36 +1,84 @@
 const express = require('express');
-
+const fs = require('fs');
+const mysql = require('mysql2');
 const router = express.Router();
-
-
-router.get('/' , (req,res)=>{
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+router.get('/', (req, res) => {
     res.render('index.hbs')
 })
-router.get('/about', (req,res)=>{
+router.get('/about', (req, res) => {
     res.render('about.hbs');
 })
-router.get('/register' , (req,res)=>{
+router.get('/register', (req, res) => {
     res.render('register.hbs')
 })
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
     res.render('login.hbs')
 })
-router.get('/home' ,(req,res)=>{
-    if(req.session.userId){
-        return res.render('home.hbs');
+router.get('/home', (req, res) => {
+    if (req.session.userId) {
+        var name, image;
+        db.query("select * from users where email = ?", [req.session.userId], (err, result) => {
+            name = result[0].name;
+            image = result[0].image;
+            console.log('name and image ' + name);
+            console.log('name and image ' + image);
+            return res.render('home.hbs', { name, image });
+        })
+    } else {
+        res.redirect('/');
     }
-    res.redirect('/');
 })
 
-router.get('/resume',(req,res)=>{
-    if(req.session.userId){
-        return res.render('resume.hbs');
+router.get('/a', (req, res) => {
+    if (req.session.userId) {
+        var name, image;
+        db.query("select * from users where email = ?", [req.session.userId], (err, result) => {
+            name = result[0].name;
+            image = result[0].image;
+            console.log('name and image ' + name);
+            console.log('name and image ' + image);
+            fs.readFile('C:/Users/saura/Desktop/Resume Creator App/SQL.txt', 'utf8', (err, data) => {
+                if(err){
+                    console.log(err);
+                }
+                console.log(data);
+                return res.render('a.hbs', { data, name, image });
+            })
+            // return res.render('resume.hbs', { name, image });
+        })
+    } else {
+        res.redirect('/')
     }
-    res.redirect('/');
 })
-router.get('/logout' , (req,res)=>{
-    // res.clearCookie('jwt');
+
+router.get('/resume', (req, res) => {
+    if (req.session.userId) {
+        var name, image;
+        db.query("select * from users where email = ?", [req.session.userId], (err, result) => {
+            name = result[0].name;
+            image = result[0].image;
+            console.log('name and image ' + name);
+            console.log('name and image ' + image);
+            return res.render('resume.hbs', { name, image });
+        })
+    } else {
+        res.redirect('/')
+    }
+})
+router.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 })
+router.get('*', (req, res) => {
+    res.redirect('/');
+})
+
+
+
 module.exports = router;
